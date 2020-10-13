6e2fdafff430cd8fcc58034ae0c3c1f197c832e0
paperbenni <paperbenni@gmail.com>
2020-10-13 22:09:36 +0200

    fix quickmenu client

--- programs/quickmenu.sh
+++ programs/quickmenu.sh
@@ -2,8 +2,7 @@
 
 # a basic client for instantos quickmenu
 
-if ! [ -e ~/.config/instantos/quickmenu ]
-then
+if ! [ -e ~/.config/instantos/quickmenu ]; then
     mkdir -p ~/.config/instantos/
     pushd ~/.config/instantos/
     git clone --depth=1 https://github.com/paperbenni/quickmenus quickmenu
@@ -15,22 +14,20 @@ cd ~/.config/instantos/quickmenu
 
 unset CHOICE
 
-while [ -z "$CHOICE" ]
-do
-    CHOICE="$(ls | fzf)"
+while :; do
+    CHOICE="$(ls -p | fzf)"
     {
-        [ -z "$CHOICE" ] || ! [ -e "$CHOICE" ] 
+        [ -z "$CHOICE" ] || ! [ -e "$CHOICE" ]
     } && exit
-    if [ -d "$CHOICE" ]
-    then
-        cd "$CHOICE"
-        continue
-    fi
-    if [ -e "$CHOICE" ]
-    then
-        "$(realpath "$CHOICE")"
+
+    if [ -d "$CHOICE" ]; then
+        cd "$CHOICE" || exit 1
+    elif [ -e "$CHOICE" ]; then
+        PCHOICE="$(realpath "$CHOICE")"
+        echo "$PCHOICE"
+        chmod +x "$PCHOICE"
+        eval "$PCHOICE"
         exit
     fi
-    
-    
+
 done
