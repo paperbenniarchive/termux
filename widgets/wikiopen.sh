287fbfef268860fec1c4fac55b411acdbbccd38b
paperbenni <paperbenni@gmail.com>
2021-07-24 01:03:22 +0200

    add directory support to wikiopen

--- widgets/wikiopen.sh
+++ widgets/wikiopen.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 # open a file from the wiki
 
-cd ~/wiki/vimwiki || {
-    echo "no wiki found"
-    sleep 4
-    exit 1
+openpath() {
+
+    cd "$1" || {
+        echo "error opening $1"
+        sleep 4
+        return 1
+    }
+
+    CHOICE="$(
+        fd . | fzf
+    )"
+
+    [ -z "$CHOICE" ] && exit
+    if ! [ -e "$CHOICE" ]; then
+        return 1
+    fi
+
+    if [ -d "$CHOICE" ]; then
+        openpath "$(realpath "$CHOICE")"
+        return
+    else
+        if grep '\.wiki$' "$CHOICE"; then
+            nvim "$CHOICE"
+        else
+            termux-open "$CHOICE"
+        fi
+    fi
+
 }
 
-CHOICE="$(
-    fd . | fzf
-)"
-
-[ -z "$CHOICE" ] && exit
-
-if grep '\.wiki$' "$CHOICE"; then
-    nvim "$CHOICE"
-else
-    termux-open "$CHOICE"
-fi
+openpath "$HOME/wiki"
